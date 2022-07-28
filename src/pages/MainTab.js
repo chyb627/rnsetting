36a731f744c5177a2fbad10a422880f51961d916
cha <chyb627@naver.com>
2022-07-28 22:22:18 +0900

    react-native-vector-icons설치 및 바텀탭 아이콘 적용

--- src/pages/MainTab.js
+++ src/pages/MainTab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import FirstScreen from './FirstScreen';
 import SecondScreen from './SecondScreen';
 import ThirdScreen from './ThirdScreen';
@@ -9,9 +10,21 @@ const Tab = createBottomTabNavigator();
 function MainTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="First" component={FirstScreen} />
-      <Tab.Screen name="Second" component={SecondScreen} />
-      <Tab.Screen name="Third" component={ThirdScreen} />
+      <Tab.Screen
+        name="First"
+        component={FirstScreen}
+        options={{ title: '홈', tabBarIcon: ({ color, size }) => <Icon name="home" color={color} size={size} /> }}
+      />
+      <Tab.Screen
+        name="Second"
+        component={SecondScreen}
+        options={{ title: '알림', tabBarIcon: ({ color, size }) => <Icon name="notifications" color={color} size={size} /> }}
+      />
+      <Tab.Screen
+        name="Third"
+        component={ThirdScreen}
+        options={{ title: '메세지', tabBarIcon: ({ color, size }) => <Icon name="message" color={color} size={size} /> }}
+      />
     </Tab.Navigator>
   );
 }
